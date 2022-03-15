11b3d175902dce422e69043ba339e49ff203cac3
Ehsan <JMSEhsan@gmail.com>
2022-03-15 11:09:59 -0600

    commit

--- Python_LICourse.py
+++ Python_LICourse.py
@@ -1,8 +1,14 @@
 # all(iterable)
 print(all(['r', '1', []]))
 print(all(['2', '']))
+print(all(['h', 0]))
+print(all(['h', {}]))
 print(all([0]))
-
-print(all([]))
 print(all(['']))
-
+print('----')
+print(all([]))    #True
+print(all({}))    #True
+print(all(''))    #True
+print('----')
+print(all('0'))
+print(all(['h', '0', 3]))
